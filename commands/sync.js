a07da4a6e79e3c45df9081b1c72d7cad11d1b8c0
Danny Guzman <guzmanjd86@gmail.com>
2024-03-06 19:28:09 -0800

    Any parent page/posts are now sync'd

--- commands/sync.js
+++ commands/sync.js
@@ -14,14 +14,49 @@ import {
     getTaxonomies,
     createTaxonomies
 } from '../lib/index.js';
+import { get } from 'http';
 
 const errors = [];
 
+/**
+ * Return all parent items for a given set of ids.
+ *
+ * @async
+ * @param {Array} ids
+ * @param {*} request
+ * @param {string} [tax='pages']
+ * @returns {unknown}
+ */
+async function getParentItems( objects, request, tax = 'pages' ){
+    let parentItemsObjects = [];
+    let objectParentIds = objects.map((obj) => { return obj.parent; });
+
+    while( objectParentIds.length > 0 ){
+
+        // if we have parent ids, we have to collect any parent items.
+        let parentItems = await getTaxonomies({
+            ...request, 
+            orderby: 'parent',
+            embed: true,
+            include: objectParentIds
+        }, tax);
+
+        // if we have parent items, we have to add to the items array.
+        if( parentItems ){
+            parentItemsObjects = parentItemsObjects.concat( parentItems );
+        }
+
+        // if the parent items have parent ids, we have to save those for the next run.
+        objectParentIds = parentItems.map((obj) => { return obj.parent; }).filter((id) => { return id !== 0; })   
+    }
+
+    return objects.concat( parentItemsObjects ).sort( (a,b) => a.parent - b.parent );
+}
 
 /**
  * Sync Environments.
  * 
- * @see https://developer.wordpress.org/rest-api/reference/settings/
+ * @see https://developer.wordpress.org/rest-api/reference/
  * 
  * @param {Object}  options
  * @param {Object}  options.spinner A CLI spinner which indicates progress.
@@ -29,6 +64,7 @@ const errors = [];
  * @param {boolean} options.to   Destination Site URL that should be synced.
  * @param {boolean} options.debug   True if debug mode is enabled.
  * @param {Array} options.tax   Taxonomy that should be synced.
+ * @param {Array} options.include   Include specific IDs only.
  */
 export default async function sync({
 	spinner,
@@ -36,7 +72,7 @@ export default async function sync({
 	from,
     to,
     tax,
-    id
+    include
 } ) {
 
     const localFile = path.join(appPath, 'caweb.json');
@@ -79,8 +115,7 @@ export default async function sync({
         url: from.url,
         headers: {
             Authorization: 'Basic ' + Buffer.from(`${from.user}:${from.pwd}`).toString('base64')
-        },
-        include: tax && id ? id.join(',') : null // if tax and id are set then we only want to get the specific items.
+        }
     }
     
 
@@ -106,25 +141,15 @@ export default async function sync({
      * 5) We only collect menus if the taxonomy is undefined or it's set to menus.
      *    - We also collect menu items if menus are collected.
      */
+    let settings = [];
+    let media = [];
     let pages = [];
     let posts = [];
-    let media = [];
-
     let menus = [];
     let menuNavItems = [];
 
-    // Site Settings.
-    spinner.text = `Getting Site Settings from ${from.url}`;
-    let settings = await getTaxonomies({
-        ...fromOptions,
-        fields: [
-            'show_on_front',
-            'page_on_front',
-            'posts_per_page'
-        ],
-    }, 'settings')
-    
     // Media Library.
+    spinner.text = `Collecting Media Library ${from.url}`;
     let mediaLibrary = await getTaxonomies({ 
             ...fromOptions, 
             fields: [
@@ -135,24 +160,73 @@ export default async function sync({
                 'alt_text', 
                 'date',
                 'mime_type',
-                'post'
-            ]
+                'post',
+                'media_details'
+            ],
+            include: tax.includes('media') && include ? include.join(',') : null
         }, 
         'media'
     );
 
+    // Site Settings.
+    if( undefined === tax || tax.includes('settings') ){
+        spinner.text = `Collecting Site Settings from ${from.url}`;
+        settings = await getTaxonomies({
+            ...fromOptions,
+            fields: [
+                'show_on_front',
+                'page_on_front',
+                'posts_per_page'
+            ],
+        }, 'settings')
+        
+    }
+
     // Pages.
     if( undefined === tax || tax.includes('pages') ){
         // get all pages/posts
-        spinner.text = `Gathering all pages from ${from.url}`;
-        pages = await getTaxonomies({...fromOptions, orderby: 'parent'}, 'pages');
+        spinner.text = `Collecting pages from ${from.url}`;
+        pages = await getTaxonomies({
+                ...fromOptions, 
+                orderby: 'parent',
+                embed: true,
+                include: tax && include ? include.join(',') : null
+            }, 
+            'pages'
+        );
+
+        // we only do this if specific ids were requested.
+        if( include ){
+            // if we have parent ids, we have to collect any parent items.
+            pages = await getParentItems( 
+                pages, 
+                fromOptions, 
+                'pages'
+            ) 
+        }
     }
 
     // Posts.
     if( undefined === tax || tax.includes('posts') ){
         // get all pages/posts
-        spinner.text = `Gathering all posts from ${from.url}`;
-        posts = await getTaxonomies({...fromOptions, orderby: 'parent'}, 'posts');
+        spinner.text = `Collecting all posts from ${from.url}`;
+        posts = await getTaxonomies({
+                ...fromOptions, 
+                orderby: 'parent',
+                include: tax && include ? include.join(',') : null
+            }, 
+            'posts'
+        );
+
+        // we only do this if specific ids were requested.
+        if( include ){
+            // if we have parent ids, we have to collect any parent items.
+            posts = await getParentItems( 
+                posts, 
+                fromOptions, 
+                'posts'
+            ) 
+        }
     }
 
     /**
@@ -165,7 +239,10 @@ export default async function sync({
      */
     for( let m of mediaLibrary ){
         // check if the media is attached to a page.
-        if( m.post ){
+        // if tax is undefined, we collect all media attached to posts and pages.
+        // if tax is defined, we only collect media attached to posts and pages if the id match.
+        if( ( ! tax && m.post ) ||
+             ( tax && include.includes(m.id.toString()) ) ){
             media.push( m );
 
             // we don't have to check any further.
@@ -180,7 +257,7 @@ export default async function sync({
             }
         }
     }
-
+    
     // filter any duplicate media.
     media = media.filter((m, index, self) => { return index === self.findIndex((t) => { return t.id === m.id; })} );
     
@@ -203,8 +280,14 @@ export default async function sync({
             m.data = mediaBlob;
         }
     }
+    
+    // this has to be done after we have the media data.
+    // Lets replace the url references in the content of the pages and posts.
+    for( let p of [].concat( pages, posts ) ){
+        p.content.rendered = p.content.rendered.replace( new RegExp(from.url, 'g'), to.url );
+    }
 
-
+    
     // Menu and Nav Items.
     if( undefined === tax || tax.includes('menus')){
         spinner.text = `Collecting assigned navigation menus from ${from.url}`;
@@ -218,7 +301,8 @@ export default async function sync({
                 'slug',
                 'meta',
                 'locations'
-            ]
+            ],
+            include: tax && include ? include.join(',') : null
             }, 'menus');
     
         menuNavItems = await getTaxonomies( 
@@ -243,6 +327,7 @@ export default async function sync({
                     'meta',
                     'menus'
                 ],
+                include: tax && include ? include.join(',') : null,
                 menus: menus.map((menu) => { return menu.id; })
             }, 
             'menu-items'
@@ -262,7 +347,7 @@ export default async function sync({
      */
 
     // Media.
-    if( undefined === tax || tax.includes('media')){
+    if( media ){
         spinner.text = `Uploading media files to ${to.url}`;
         await createTaxonomies(
             media.filter((img) => { return img.data }), 
@@ -273,27 +358,30 @@ export default async function sync({
     }
 
      // Pages.
-    if( undefined === tax || tax.includes('pages')){
+    if( pages ){
         spinner.text = `Creating all pages to ${to.url}`;
         await createTaxonomies( pages, toOptions, 'pages', spinner );
     }
 
      // Posts.
-    if( undefined === tax || tax.includes('posts')){
+    if( posts ){
         spinner.text = `Creating all posts to ${to.url}`;
         await createTaxonomies( posts, toOptions, 'posts', spinner );
     }
 
     // Menus and Navigation Links.
-    if( undefined === tax || tax.includes('menus')){
+    if( menus ){
         spinner.text = `Reconstructing navigation menus to ${to.url}`;
         await createTaxonomies(menus, toOptions, 'menus', spinner);
         await createTaxonomies(menuNavItems, toOptions, 'menu-items', spinner);
     }
     
 
-    // update settings
-    await createTaxonomies(settings, toOptions, 'settings', spinner);
+    // Settings.
+    if( settings ){
+        spinner.text = `Updating site settings to ${to.url}`;
+        await createTaxonomies(settings, toOptions, 'settings', spinner);
+    }
 
     spinner.text = `Sync from ${from.url} to ${to.url} completed successfully.`
 
