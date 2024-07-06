bddd63d4fabb0721ee70584646bdabe5d53330a1
Danny Guzman <guzmanjd86@gmail.com>
2024-07-06 01:07:24 -0700

    Added new commands reference sheet

--- docs/tool/index.js
+++ docs/tool/index.js
@@ -1,21 +1,38 @@
-'use strict';
+#!/usr/bin/env node
 
 
 /**
  * External dependencies
  */
 import path from 'node:path';
-import fs from 'fs-extra';
+import fs from 'fs';
 
 import { CAWEB_OPTIONS, DIVI_OPTIONS } from '../../lib/index.js';
+import cli from '../../lib/cli.js';
 
-generateOverridesMD()
+
+generateCommandsMD();
+generateOverridesMD();
+
+
+/**
+ * Replace any characters that need to be encoded.
+ *
+ * @param {String} str Text to encode.
+ * @returns {String}
+ */
+function encodedString(str){
+	return str
+		.replace('<','&lt;')
+		.replace('>','&gt;')
+		.replace(/\n/g, `  \n`)
+}
 
 /**
  * Generates the OVERRIDES.MD
  * 
  */
-export default async function generateOverridesMD() {
+async function generateOverridesMD() {
 
 	let output = [
 		'## [.wp-env.override.json](https://developer.wordpress.org/block-editor/reference-guides/packages/packages-env/#wp-env-override-json)',
@@ -43,3 +60,79 @@ export default async function generateOverridesMD() {
 	);
 
 };
+
+
+async function generateCommandsMD() {
+
+	let commands = []
+
+	cli().commands.forEach( ( c ) => {
+		commands[c.name()] = [
+			`### \`caweb ${c.name()}\``,
+			encodedString(c.description()),
+			'<pre>',
+			`<b>Usage:</b> caweb ${c.name()} ${encodedString(c.usage())}`
+		]
+
+		// Add arguments
+		if( c.registeredArguments.length ){
+			commands[c.name()].push('\n<b>Arguments:</b>')
+			c.registeredArguments.forEach(( arg ) => {
+				commands[c.name()].push( addOption( {...arg, flags: arg.name()} ) )
+			})
+		}
+
+		// Add options
+		if( c.options.length ){
+			commands[c.name()].push('\n<b>Options:</b>')
+			c.options.forEach(( opt ) => {
+				commands[c.name()].push( addOption( opt ) )
+			})
+		}
+
+		commands[c.name()].push('</pre>')
+	})
+
+	// Sort commands
+	commands = Object.keys(commands).sort().reduce((temp, key) => {
+		temp = [].concat(temp, commands[key]);
+
+		return temp;
+	}, [])
+	
+	fs.writeFileSync(
+		path.join(process.cwd(), 'docs', 'COMMANDS.MD'),
+		['## Command Reference', ...commands].join('\n')
+	);
+
+};
+
+function addOption({
+	flags,
+	description,
+	defaultValue,
+	argChoices
+}){
+	const minPadding = 35;
+	let padding = minPadding +
+		(flags.includes('<') ? 3 : 0) +
+		(flags.includes('>') ? 3 : 0);
+
+	flags = encodedString(flags);
+
+	let output = `${flags.padEnd(padding, ' ')}${encodedString(description)}`;
+
+	// Add Default Value 
+	if( undefined !== defaultValue ){
+		output = `${output}\n${' '.repeat(minPadding) }Default: ${defaultValue}`
+	}
+	
+	// Add Choices
+	if( undefined !== argChoices ){
+		output = `${output}\n${' '.repeat(minPadding) }Choices: ${argChoices}`
+	}
+
+	return output;
+}
+
+export default generateOverridesMD; 
